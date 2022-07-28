f3e9dc02185350fb0fce003d0d2740cff0094ee7
José Morales <jmoralz92@gmail.com>
2022-07-28 00:11:48 -0500

    migrate to nbdev2

--- action_files/black.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# __all__ gets written by nbdev and formatting everytime is annoying
-# we just skip checking __all__ altogether
-for FILE in $(find mlforecast -name "[!_]*.py"); do
-  echo "Linting $FILE"
-  START=$(grep -n "Cell" $FILE | head -n 1 | cut -d : -f 1)
-  tail -n +$START $FILE | black --check -S - || exit -1
-done
