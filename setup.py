8c8a5b298e81d641f68b0e5c35fb29f7c7c11494
José Morales <jmoralz92@gmail.com>
2022-08-17 23:09:29 -0500

    update templates

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ py_versions = '2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7 3.0 3.1 3.2 3.3 3.4 3.5 3.6 3.7 3
 
 requirements = cfg.get('requirements','').split()
 distributed_requirements = cfg.get('distributed_requirements', '').split()
-dev_requirements = cfg.get('dev_requirements', '').split() + distributed_requirements
+dev_requirements = requirements + distributed_requirements + cfg.get('dev_requirements', '').split()
 min_python = cfg['min_python']
 lic = licenses.get(cfg['license'].lower(), (cfg['license'], None))
 
