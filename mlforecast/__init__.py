a6d71cbce4706e3f03d5c61856739aca3fb124e6
José Morales <jmoralz92@gmail.com>
2022-11-24 21:30:04 -0600

    rename forecast to mlforecast (#63)

--- mlforecast/__init__.py
+++ mlforecast/__init__.py
@@ -1,4 +1,3 @@
 __version__ = "0.3.1"
-__all__ = ['Forecast', 'TimeSeries']
-from mlforecast.core import TimeSeries
-from mlforecast.forecast import Forecast
+__all__ = ['Forecast', 'MLForecast']
+from mlforecast.forecast import Forecast, MLForecast
