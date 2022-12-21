9f2903a11da92c8236b0df2fa9fc6b28c5740e95
FedericoGarza <fede.garza.ramirez@gmail.com>
2022-12-21 16:52:13 -0600

    feat: add warning about step size behavior (distributed forecast)

--- mlforecast/distributed/forecast.py
+++ mlforecast/distributed/forecast.py
@@ -296,6 +296,15 @@ class DistributedMLForecast:
         result : dask DataFrame
             Predictions for each window with the series id, timestamp, last train date, target value and predictions from each model.
         """
+        if window_size != step_size:
+            warning_msg = (
+                "The gap between each cross validation window (controled by `step_size`) "
+                "changed from being equal to the `window_size` to being equal to 1. "
+                "Please set `step_size` equal to the value of `window_size` to mantain "
+                "the old behavior."
+            )
+            warnings.warn(warning_msg, RuntimeWarning)
+
         results = []
         self.cv_models_ = []
         if id_col != "index":
