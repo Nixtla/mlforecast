8e9432a5bc5b6c47cccf65225f90d150b7be29c9
José Morales <jmoralz92@gmail.com>
2023-02-02 20:44:05 -0600

    remove lightgbm import from project namespace (#88)

--- mlforecast/forecast.py
+++ mlforecast/forecast.py
@@ -6,7 +6,7 @@ __all__ = ['MLForecast', 'Forecast']
 # %% ../nbs/forecast.ipynb 3
 import copy
 import warnings
-from typing import Callable, Dict, Iterable, List, Optional, Tuple, Union
+from typing import TYPE_CHECKING, Callable, Dict, Iterable, List, Optional, Tuple, Union
 
 import numpy as np
 import pandas as pd
@@ -22,7 +22,9 @@ from mlforecast.core import (
     TimeSeries,
     _name_models,
 )
-from .lgb_cv import LightGBMCV
+
+if TYPE_CHECKING:
+    from mlforecast.lgb_cv import LightGBMCV
 from .utils import backtest_splits, _cotransform, PredictionIntervals
 
 # %% ../nbs/forecast.ipynb 6
@@ -139,7 +141,7 @@ class MLForecast:
         return self.ts.freq
 
     @classmethod
-    def from_cv(cls, cv: LightGBMCV) -> "MLForecast":
+    def from_cv(cls, cv: "LightGBMCV") -> "MLForecast":
         if not hasattr(cv, "best_iteration_"):
             raise ValueError("LightGBMCV object must be fitted first.")
         import lightgbm as lgb
