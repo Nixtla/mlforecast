9d8df2af78cea367306f845044f3d43330c1aae0
José Morales <jmoralz92@gmail.com>
2023-08-16 17:56:40 -0600

    lint

--- mlforecast/target_transforms.py
+++ mlforecast/target_transforms.py
@@ -10,7 +10,7 @@ from typing import Iterable, Optional
 
 import numpy as np
 import pandas as pd
-from sklearn.base import BaseEstimator, TransformerMixin, clone
+from sklearn.base import TransformerMixin, clone
 from numba import njit
 
 from .grouped_array import GroupedArray, _apply_difference
