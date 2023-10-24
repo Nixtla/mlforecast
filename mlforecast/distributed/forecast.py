c04025c56d9652726dd9ce122f5fe5b5da4bf7ff
José Morales <jmoralz92@gmail.com>
2023-10-24 12:25:40 -0600

    Fix distributed cv (#254)

--- mlforecast/distributed/forecast.py
+++ mlforecast/distributed/forecast.py
@@ -59,7 +59,7 @@ class DistributedMLForecast:
     def __init__(
         self,
         models,
-        freq: Optional[Freq] = None,
+        freq: Freq,
         lags: Optional[Lags] = None,
         lag_transforms: Optional[LagTransforms] = None,
         date_features: Optional[Iterable[DateFeature]] = None,
@@ -615,8 +615,4 @@ class DistributedMLForecast:
                 engine=self.engine,
             )
             results.append(fa.get_native_as_df(preds))
-        if len(results) == 1:
-            return results[0]
-        if len(results) == 2:
-            return fa.union(results[0], results[1])
-        return fa.union(results[0], results[1], results[2:])
+        return fa.union(*results)
