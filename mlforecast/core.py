04ddad15ec1c83b41c9594af032a9e8dad38977b
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-11-17 15:50:01 +0000

    thousands separator

--- mlforecast/core.py
+++ mlforecast/core.py
@@ -752,7 +752,7 @@ class TimeSeries:
                     "Found missing inputs in X_df. "
                     "It should have one row per id and date for the complete forecasting horizon.\n"
                     f"Got: {X_df.shape[0]} rows, expected: {len(self._uids) * horizon} rows "
-                    f"(horizon={horizon}, n_ids={len(self._uids)})."
+                    f"(horizon={horizon:,}, n_ids={len(self._uids):,})."
                 )
                 raise ValueError(msg)
             drop_cols = [self.id_col, self.time_col, "_start", "_end"]
