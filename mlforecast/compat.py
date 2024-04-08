b771ae16e65c984f5504f9124e8a84ad32313765
Tyler Blume <t-blume@hotmail.com>
2024-04-08 16:41:29 -0400

    add auto module for hyperparameter optimization (#306)

--- mlforecast/compat.py
+++ mlforecast/compat.py
@@ -5,8 +5,35 @@ __all__ = []
 
 # %% ../nbs/compat.ipynb 1
 try:
+    from catboost import CatBoostRegressor
+except ImportError:
+
+    class CatBoostRegressor:
+        def __init__(self, *args, **kwargs):  # noqa: ARG002
+            raise ImportError("Please install catboost to use this model.")
+
+
+try:
+    from lightgbm import LGBMRegressor
+except ImportError:
+
+    class LGBMRegressor:
+        def __init__(self, *args, **kwargs):  # noqa: ARG002
+            raise ImportError("Please install lightgbm to use this model.")
+
+
+try:
     from window_ops.shift import shift_array
 except ImportError:
 
-    def shift_array(*_args, **_kwargs):
+    def shift_array(*_args, **_kwargs):  # noqa: ARG002
         raise Exception
+
+
+try:
+    from xgboost import XGBRegressor
+except ImportError:
+
+    class XGBRegressor:
+        def __init__(self, *args, **kwargs):  # noqa: ARG002
+            raise ImportError("Please install xgboost to use this model.")
