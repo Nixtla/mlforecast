e45e54807a13b485c174e3df55c8a0da9044ffc2
Saul Caballero <saul.caballero.ramirez@gmail.com>
2025-12-30 16:02:36 -0600

    Adds test for Ray xgboost

--- tests/distributed_ray/test_ray_forecast.py
+++ tests/distributed_ray/test_ray_forecast.py
@@ -7,6 +7,7 @@ import ray
 
 from mlforecast.distributed import DistributedMLForecast
 from mlforecast.distributed.models.ray.lgb import RayLGBMForecast
+from mlforecast.distributed.models.ray.xgb import RayXGBForecast
 from mlforecast.lag_transforms import ExpandingMean, RollingMean
 from mlforecast.utils import generate_daily_series
 
@@ -15,7 +16,15 @@ warnings.simplefilter("ignore", FutureWarning)
 
 @pytest.mark.ray
 @pytest.mark.skipif(sys.version_info <= (3, 9), reason="Distributed tests are not supported on Python < 3.10")
-def test_ray_distributed_forecast():
+@pytest.mark.parametrize(
+    "model_class,model_kwargs",
+    [
+        (RayLGBMForecast, {"verbosity": -1, "random_state": 0}),
+        (RayXGBForecast, {"random_state": 0, "n_jobs": 1}),
+    ],
+    ids=["lightgbm", "xgboost"]
+)
+def test_ray_distributed_forecast(model_class, model_kwargs):
     series = generate_daily_series(
         100, equal_ends=True, min_length=500, max_length=1_000
     )
@@ -29,7 +38,7 @@ def test_ray_distributed_forecast():
 
     # test existing features provide the same result
     fcst = DistributedMLForecast(
-        models=[RayLGBMForecast(verbosity=-1, random_state=0)],
+        models=[model_class(**model_kwargs)],
         freq="D",
         lags=[1, 2, 3, 4, 5, 6, 7],
         lag_transforms={
@@ -44,7 +53,7 @@ def test_ray_distributed_forecast():
     )
     fcst.fit(training_df_featured, static_features=[], dropna=False)
     preds1 = fcst.predict(10).to_pandas().sort_values(["unique_id", "ds"], ignore_index = True)
-    fcst.save('/tmp/test_ray_forecast_model.pkl')
+    fcst.save(f'/tmp/test_ray_forecast_model_{model_class.__name__}.pkl')
 
     # df without features
     fcst.preprocess(ray_dataset, static_features=[], dropna=False)
